5932f35e995c6ffe9a41355685902a2776491e29
Morfax <dota2latinoamericamx@gmaill.com>
2018-07-04 14:52:15 -0500

    v1.2

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,25 @@
 var express = require('express');
 var router = express.Router();
+var fs = require('fs');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  var obj;
+  fs.readFile('./db/test.JSON', 'utf8', function (err, data) {
+    if (err) throw err;
+    obj = JSON.parse(data);
+    console.log(obj.page.menus);
+    res.render('index', { title: obj.page.title, conten: obj.page.conten, menus: obj.page.menus, pagina: "index" });
+  });
+});
+
+router.get('/productos', function(req, res, next) {
+  var obj;
+  fs.readFile('./db/test.JSON', 'utf8', function (err, data) {
+    if (err) throw err;
+    obj = JSON.parse(data);
+    res.render('index', { title: obj.page.title, conten: obj.page.conten, menus: obj.page.menus, productos: obj.page.productos, pagina: "productos" });
+  });
 });
 
 module.exports = router;
