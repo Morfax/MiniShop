ec020e8a00df2b8d3cafa7088633412af48079e7
Morfax <dota2latinoamericamx@gmaill.com>
2018-07-13 16:58:57 -0500

    v1.6

--- app.js
+++ app.js
@@ -17,7 +17,7 @@ mongoose.connect('mongodb://' + process.env.DB_HOST, { useNewUrlParser: true })
 });
 
 var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+var productosRouter = require('./routes/productos');
 
 var app = express();
 
@@ -32,7 +32,7 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
-app.use('/users', usersRouter);
+app.use('/productos', productosRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
--- routes/productos.js
+++ routes/productos.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var router = express.Router();
 var fs = require('fs');
+var Producto = require('../db/schemas/productos');
 
 fs.readFile('./db/structure.JSON', 'utf8', function (err, data) {
   if (err) throw err;
@@ -8,7 +9,10 @@ fs.readFile('./db/structure.JSON', 'utf8', function (err, data) {
 });
 
 router.get('/', function(req, res, next) {
-    res.render('index', { page: obj.page, pagina: "index" });
+  Producto.find().exec(function(err, productos) {
+      if (err) throw err;
+      res.render('index', { page: obj.page, pagina: "productos", productos: productos }); 
+  });
 });
 
 module.exports = router;
