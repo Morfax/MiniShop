a1a156a8142a60641c5c0134149953a76987e981
Morfax <eliseo_sy@hotmail.com>
2018-07-22 23:27:15 -0500

    v1.8

--- routes/productos.js
+++ routes/productos.js
@@ -9,9 +9,29 @@ fs.readFile('./db/structure.JSON', 'utf8', function (err, data) {
 });
 
 router.get('/', function(req, res, next) {
+    res.render('index', { page: obj.page, pagina: "productos" }); 
+});
+
+router.get('/listas', function(req, res, next) {
   Producto.find().exec(function(err, productos) {
       if (err) throw err;
-      res.render('index', { page: obj.page, pagina: "productos", productos: productos }); 
+      res.render('index', { page: obj.page, pagina: "productoslista", productos: productos }); 
+  });
+});
+
+router.get('/agregar', function(req, res, next) {
+  Producto.find().exec(function(err, productos) {
+      if (err) throw err;
+      res.render('index', { page: obj.page, pagina: "productosagregar", productos: productos }); 
+  });
+});
+
+router.post('/agregar', function(req, res, next) {
+  console.log(req.body);
+  var articulo = new Producto({ name: req.body.productoNombre, cost: req.body.productoCosto });
+  articulo.save(function (err) {
+    if (err) return handleError(err);
+    res.redirect('./listas');
   });
 });
 
